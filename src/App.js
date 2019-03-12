b24feb254bb06341a03589eaa3f37cb8e04db4cc
Rytchet <oskarino.g@gmail.com>
2019-03-12 22:52:23 +0000

    Change all todos to use UUID

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
 import axios from "axios";
+import uuid from "uuid";
 
 import "./App.css";
 
@@ -17,7 +18,16 @@ class App extends Component {
   componentDidMount() {
     axios
       .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(res => this.setState({ todos: res.data }));
+      .then(res => {
+        this.setState({
+          todos: res.data.map(todo => {
+            // Use uuid instead of the server response id to be consistent
+            // with own added todos
+            todo.id = uuid.v4();
+            return todo;
+          })
+        });
+      });
   }
 
   toggleComplete = id => {
@@ -45,7 +55,14 @@ class App extends Component {
         title,
         completed: false
       })
-      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+      .then(res => {
+        // It has to use uuid instead of the server response because it's
+        // always the same, and it breaks the delete / toogleComplete and
+        // gives errors in React. With a real server it wouldn't have to do
+        // that because all id's would be unique
+        res.data.id = uuid.v4();
+        this.setState({ todos: [...this.state.todos, res.data] });
+      });
   };
 
   render() {
